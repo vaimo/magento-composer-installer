663078e81ecd34ee1f5a77105c6774c99e43197d
Ivan Gavryshko <igavryshko@ebay.com>
2015-03-03 14:29:26 -0600

    MAGETWO-34669: Removing unnesessary data during installaiton Magento
    - fixed generated comment.

--- src/MagentoHackathon/Composer/Magento/Plugin.php
+++ src/MagentoHackathon/Composer/Magento/Plugin.php
@@ -259,6 +259,11 @@ class Plugin implements PluginInterface, EventSubscriberInterface
         $content = <<<AUTOLOAD
 <?php
 /**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+/**
  * Path to Composer vendor directory
  */
 return '$vendorDirPath';
